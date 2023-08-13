8ef771029094650430a68643245e68ea2dcb86f4
MOHDZAMA <106185773+MOHDZAMA@users.noreply.github.com>
2023-08-13 15:21:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,48 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask, render_template, Response
+
+from camera import VideoCamera
+
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return render_template("index.html")
+
+
+@app.route("/dashbord.html")
+def dashbord():
+    return render_template("dashbord.html")
+
+
+@app.route("/exercise_select.html")
+def exercise_select():
+    return render_template("exercise_select.html")
+
+
+@app.route("/planworkout.html")
+def planworkout():
+    return render_template("planworkout.html")
+
+
+@app.route('/shoulder.html')
+def sample():
+    return render_template('shoulder.html')
+
+
+@app.route('/video_feed1')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type : image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
 
 
 if __name__ == '__main__':
